a7ab62cf6517ba74eec88a14a8baa3946ad588d2
Firas Khemiri <firas.khemiri@esprit.tn>
2019-04-16 23:37:18 +0100

    Update

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -70,6 +77,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -83,8 +93,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        "ENGINE": 'django.db.backends.postgresql',
+        'NAME': 'campus',
+        'USER': 'Firas@campus',
+        'PASSWORD': 'Lenovo y 530',
+        'HOST': 'campus.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
@@ -125,4 +142,63 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'customUser.User'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
+    ]
+}
+
+AUTHENTICATION_BACKENDS = (
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+
+    'social_core.backends.facebook.FacebookAppOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+)
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1823460334559586'
+SOCIAL_AUTH_FACEBOOK_SECRET = 'ce53c420a7b953c5d9c6b5788d9af9c3'
+
+# Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from facebook. Email is not sent by default, to get it, you must request the email permission:
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['public_profile','email', 'birthday', 'picture.height(300).width(300)']
+FACEBOOK_EXTENDED_PERMISSIONS = ['user_about_me','user_birthday']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id, name , birthday , picture.height(300).width(300)'
+}
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+
+    'customUser.models.save_profile',
+
+)
+
+PUSH_NOTIFICATIONS_SETTINGS = {
+
+        "CONFIG": "push_notifications.conf.AppConfig",
+
+        "APPLICATIONS": {
+        "my_fcm_app": {
+
+            "PLATFORM": "FCM",
+     #       "API_KEY": "AAAATEh95aA:APA91bGraYQ_U_SfJz9gMo6Ki453ziln07_F7novLo2H0XnoILe_Ezd4_5rLQe_vsGBLymSZnoDsZfPiaXI6Orr7_rY0KvzqKPJOXbYziicitvqlvtNAGGapaHkAf2eWEjfSK_0WbQWL",
+
+            "API_KEY": "AAAATEh95aA:APA91bFCRGHnDXR8KXvsXLSK03v3qkU7mXZgPYFHCXbtVvTQMUwCd-w3O6k-5040PJ7CVnB368WG91Fdwx2Ts26jyjlo7ctzM9ItYTnIdi6-p85vxHCFhH-pnui5vLct4ma6jXovJd8S",
+            "MAX_RECIPIENTS":1000,},
+        }
+}
