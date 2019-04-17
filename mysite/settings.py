3e0921e689905dbd4c864398004cc5b84291bbf3
Firas Khemiri <firas.khemiri@esprit.tn>
2019-04-17 03:06:15 +0100

    Update PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,15 +93,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        # "ENGINE": 'django.db.backends.postgresql',
-        # 'NAME': 'campus',
-        # 'USER': 'Firas@campus',
-        # 'PASSWORD': 'Lenovo y 530',
-        # 'HOST': 'campus.postgres.database.azure.com',
-        # 'PORT': '5432',
+        "ENGINE": 'django.db.backends.postgresql',
+        'NAME': 'campus',
+        'USER': 'Firas@campus',
+        'PASSWORD': 'Lenovo y 530',
+        'HOST': 'campus.postgres.database.azure.com',
+        'PORT': '5432',
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
