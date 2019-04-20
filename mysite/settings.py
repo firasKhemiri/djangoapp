5c37fae829a785d09cfd3b2129431002a6b5bc30
Firas Khemiri <firas.khemiri@esprit.tn>
2019-04-20 21:22:34 +0100

    Change to MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
 
-    # 'ENGINE': 'django.db.backends.mysql',
-    # 'NAME': 'campus',
-    # 'USER': 'root',
-    # 'PASSWORD': '',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'campus',
+    'USER': 'root',
+    'PASSWORD': '',
 
     # 'ENGINE': 'mysql.connector.django',
 
-    "ENGINE": 'django.db.backends.postgresql',
-    'NAME': 'campus',
-    'USER': 'Firas@campus',
-    'PASSWORD': 'Lenovo y 530',
-    'HOST': 'campus.postgres.database.azure.com',
-    'PORT': '5432',
+    # "ENGINE": 'django.db.backends.postgresql',
+    # 'NAME': 'campus',
+    # 'USER': 'Firas@campus',
+    # 'PASSWORD': 'Lenovo y 530',
+    # 'HOST': 'campus.postgres.database.azure.com',
+    # 'PORT': '5432',
     }
 }
 
